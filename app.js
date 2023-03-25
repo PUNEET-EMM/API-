00793505f4a60449ba810026a61ff3de7ac0b9b2
Puneet <puneet19338@gmail.com>
2023-03-25 13:05:47 +0530

    Third commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 var md5 = require('md5');
 const helmet = require("helmet");
-
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(bodyParser.json());
@@ -27,9 +27,7 @@ app.use(bodyParser.urlencoded({
 
 
 
-
-
-// Monogdb cnnection
+// Monogdb connection
 const userSchema = new mongoose.Schema({
   userName:String,
   email:String,
@@ -37,8 +35,110 @@ const userSchema = new mongoose.Schema({
 });
 
 
-
 const User  = new mongoose.model("User",userSchema);
+
+
+
+app.get('/',(req,res)=>{
+ res.render("home");
+})
+
+app.get('/register',(req,res)=>{
+    res.render("register");
+   })
+app.post('/register',(req,res)=>{
+    const newUser =  new User({
+        
+        userName:req.body.userName,
+        email:req.body.mail,
+        password:md5(req.body.password)
+    })
+    newUser.save();
+    res.send("Thank you for register");
+ })
+
+ app.get('/login',(req,res)=>{
+    res.render("login");
+   })
+
+   app.post("/login", async function(req,res){
+    const mail = req.body.email;
+    const password = md5(req.body.password);
+
+    User.findOne({email:mail})
+     .then((foundUser) => {
+        if(foundUser){
+             
+            if(foundUser.password === password){
+                const accessToken = generateAccessToken(foundUser);
+                console.log(accessToken);
+                
+                res.json("Access token :: " +accessToken);
+                
+                
+            }else res.send('Incorrect Password');
+        }
+   })
+   .catch((err) => {
+       
+
+console.log(err);
+       res.send(400, "Bad Request");
+   });
+      
+});
+
+
+
+
+
+  //  JWT Token
+
+
+  const generateAccessToken = (user) => {
+    return jwt.sign({ id: user.id}, process.env.KEY, {
+      expiresIn: "900s",
+    });
+  };
+
+  const verify = (req, res, next) => {
+    
+    if(req.body.token){
+      const token = req.body.token.split(" ")[1];
+
+  
+      jwt.verify(token, process.env.KEY, (err, user) => {
+        if (err) {
+          return res.status(403).json("Token is not valid!");
+        }
+  
+        req.user = user;
+        next();
+      });
+    } else {
+      res.status(401).json("You are not authenticated!");
+    }
+  };
+
+
+  app.get('/forgot',verify,async(req,res)=>{
+    res.render("forgot");
+   })
+   app.post('/forgot',async(req,res)=>{
+    const mail = req.body.email;
+    const newPassword = md5(req.body.password);
+    const doc = await User.findOneAndUpdate({emai:mail}, {password:newPassword}, {
+        new: true
+      });
+    res.send("Your password has been changed ");
+   })
+
+
+  //  Social Media  Post Crud Operation
+
+
+
+ 
 
 const PostSchema = new mongoose.Schema(
   {
@@ -64,80 +164,22 @@ const PostSchema = new mongoose.Schema(
 
 const Post = new mongoose.model("Post",PostSchema)
 
-
-
-
-
-app.get('/',(req,res)=>{
- res.render("home");
-})
-
-app.get('/register',(req,res)=>{
-    res.render("register");
-   })
-app.post('/register',(req,res)=>{
-    const newUser =  new User({
-        
-        userName:req.body.userName,
-        email:req.body.mail,
-        password:md5(req.body.password)
-    })
-    newUser.save();
-    res.send("your are login");
- })
-
- app.get('/login',(req,res)=>{
-    res.render("login");
-   })
-
-   app.post("/login",function(req,res){
-    const mail = req.body.email;
-    const password = md5(req.body.password);
-
-    User.findOne({email:mail})
-     .then((foundUser) => {
-        if(foundUser){
-            if(foundUser.password === password){
-                res.send("your are login");
-            }else res.send('Incorrect Password');
-        }
-   })
-   .catch((err) => {
-       
-
-console.log(err);
-       res.send(400, "Bad Request");
-   });
-      
-});
-
-app.get('/forgot',async(req,res)=>{
-    res.render("forgot");
-   })
-   app.post('/forgot',async(req,res)=>{
-    const mail = req.body.email;
-    const newPassword = md5(req.body.password);
-    const doc = await User.findOneAndUpdate({emai:mail}, {password:newPassword}, {
-        new: true
-      });
-    res.send("Your password has been changed ");
-   })
-
-app.post('/posts',(req,res)=>{
-  
-
+app.post('/posts',verify,(req,res)=>{
     const newPost = new Post(req.body);
        newPost.save();
        res.status(200).json(newPost);    
 });
- app.put("/posts/:id", async (req, res) => {
+
+
+
+ app.put("/posts/:id",verify, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.userName === req.body.userName) {
       await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
+      res.status(200).json("The post has been updated");
     } else {
-      res.status(403).json("you can update only your post");
+      res.status(403).json("You can update only your post");
     }
   } catch (err) {
     res.status(500).json(err);
@@ -145,7 +187,8 @@ app.post('/posts',(req,res)=>{
 });
 
 
-app.delete("/posts/:id", async (req, res) => {
+
+app.delete("/posts/:id",verify, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.userName=== req.body.userName) {
@@ -159,7 +202,8 @@ app.delete("/posts/:id", async (req, res) => {
   }
 });
 
-app.put("/posts/:id/like", async (req, res) => {
+
+app.put("/posts/:id/like",verify, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.body.userName)) {
@@ -174,7 +218,8 @@ app.put("/posts/:id/like", async (req, res) => {
   }
 });
 
-app.put("/posts/:id/comment", async (req, res) => {
+
+app.put("/posts/:id/comment", verify,async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
   
@@ -187,7 +232,7 @@ app.put("/posts/:id/comment", async (req, res) => {
 });
 
 
-app.get("/posts/:id", async (req, res) => {
+app.get("/posts/:id",verify, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
@@ -195,6 +240,8 @@ app.get("/posts/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
 
 
 app.listen(3000,()=>{
